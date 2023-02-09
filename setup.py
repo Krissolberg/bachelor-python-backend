97431b78c1357aa46dc82e0080be4862ad5cba84
Krissolberg <73477721+Krissolberg@users.noreply.github.com>
2023-02-09 15:55:29 +0100

    Updated gitignore and setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='bachelor-backend',
+    name='bachelorbackend',
     version='1.1.1',
     package=find_packages(),
     install_requires=[
-        'Twisted<=15.2.1'
+        'fastAPI',
+        'shodan',
+        'dotenv'
         ],
 )
