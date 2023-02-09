42479ae12ef893f7ad96fa953b11cf9018edb52e
Krissolberg <73477721+Krissolberg@users.noreply.github.com>
2023-02-09 11:57:19 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 import shodan
+import sys
+import os
 
-SHODAN_API_KEY = "ch0KPiOvr34Tf3It6N5EqhSjEgEuEb0F"
+from dotenv import load_dotenv
+
+load_dotenv() # take environment from .env.
+
+
+SHODAN_API_KEY = os.getenv("API_KEY")
 
 api = shodan.Shodan(SHODAN_API_KEY)
 
-# Search Shodan
-results = api.search('politiet.no')
+input = sys.argv[0]
 
-# Show the results
-print('Results found: {}'.format(results['total']))
-for result in results['matches']:
-        print('IP: {}'.format(result['ip_str']))
-        print(result['data'])
+def shodanSearch(input):
+    # Search Shodan
+    results = api.search('input')
+
+    # Show the results
+    print('Results found: {}'.format(results['total']))
+    for result in results['matches']:
+            print('IP: {}'.format(result['ip_str']))
+            print(result['data'])
+
+# Lookup the host
+host = api.host('163.174.115.13')
+
+# Print general info
+print("""
+        IP: {}
+        Organization: {}
+        Operating System: {}
+""".format(host['ip_str'], host.get('org', 'n/a'), host.get('os', 'n/a')))
+
+# Print all banners
+for item in host['data']:
+        print("""
+                Port: {}
+                Banner: {}
+
+        """.format(item['port'], item['data']))
+
