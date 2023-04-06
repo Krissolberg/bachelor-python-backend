1b12c99fe0f6d4b5ea6c999a02484857de3f5456
Sufi Y <yagci.sufi88@gmail.com>
2023-04-06 21:53:17 +0200

    Feil filstruktur for init av MongoDB, fiksa!

---
+++ docker/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mongoimport --jsonArray --db info_db --collection portBes --file /tmp/data/port.json
+mongoimport --jsonArray --db info_db --collection lederTekst --file /tmp/data/leder.json
+mongoimport --jsonArray --db info_db --collection versionBes --file /tmp/data/versTekst.json
+mongoimport --jsonArray --db info_db --collection vulnBes --file /tmp/data/vulns.json
