a41004d756b77655950b6f937c8f0fc069fe6f5e
Sufi Y <yagci.sufi88@gmail.com>
2023-04-19 03:40:50 +0200

    Fikser json filene, da de ikke var på engelsk

--- docker/init.sh
+++ docker/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mongoimport --jsonArray --db info_db --collection portBes --file /tmp/data/port.json
-mongoimport --jsonArray --db info_db --collection lederTekst --file /tmp/data/leder.json
-mongoimport --jsonArray --db info_db --collection versionBes --file /tmp/data/versTekst.json
-mongoimport --jsonArray --db info_db --collection vulnBes --file /tmp/data/vulns.json
+mongoimport --jsonArray --db info_db --collection portdes --file /tmp/data/port.json
+mongoimport --jsonArray --db info_db --collection leaderText --file /tmp/data/leader.json
+mongoimport --jsonArray --db info_db --collection versionDes --file /tmp/data/versText.json
+mongoimport --jsonArray --db info_db --collection vulnDes --file /tmp/data/vulns.json
 mongoimport --jsonArray --db users --collection user --file /tmp/data/user.json
