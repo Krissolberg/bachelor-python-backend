caa425324e490bccd24a0dd15fb054bfc3bdbfdd
Sufi Y <yagci.sufi88@gmail.com>
2023-04-25 20:34:45 +0200

    Fjerner default user

--- docker/init.sh
+++ docker/init.sh
@@ -4,4 +4,3 @@ mongoimport --jsonArray --db info_db --collection portBes --file /tmp/data/port.
 mongoimport --jsonArray --db info_db --collection lederTekst --file /tmp/data/leder.json
 mongoimport --jsonArray --db info_db --collection versionBes --file /tmp/data/versTekst.json
 mongoimport --jsonArray --db info_db --collection vulnBes --file /tmp/data/vulns.json
-mongoimport --jsonArray --db users --collection user --file /tmp/data/user.json
